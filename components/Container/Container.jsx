52d2bd0319e1fc4af4d3d404a70538382a07ec69
asdrenahmeti <asdrenahmeti95@gmail.com>
2022-06-06 17:47:53 +0200

    fix: improvements

--- components/Container/Container.jsx
+++ components/Container/Container.jsx
@@ -1,5 +1,5 @@
 function Container({ style, children }){
-  return <div className={`${style} xs:px-4 max-w-[1680px] container mx-auto`}>{children}</div>
+  return <div className={`${style} xs:px-4 max-w-[1400px] container mx-auto`}>{children}</div>
 }
 
 export default Container
