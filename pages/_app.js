fcf8be53f0f61664414cbd7570e187fe8af592e2
Asdren <asdrenahmeti95@gmail.com>
2022-07-26 22:57:34 +0200

    fix: embedded list import

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Layout from '../components/Layout/Layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  console.log(Component.name)
   if (Component.name === 'Construction') {
     return <Component {...pageProps} />
   } else {
